2270b24d6a32d4219438f6e17a6069d14f6ff833
neelarani <neela9624@gmail.com>
2024-10-14 10:58:24 +0600

    mealDB 50% done

--- js/logic.js
+++ js/logic.js
@@ -1,42 +1,95 @@
-const searchMealByName = async () => {
+// spinner show
+const loadAllMeals = async mealName => {
   const response = await fetch(
-    `https://www.themealdb.com/api/json/v1/1/search.php?f=a`
+    `https://www.themealdb.com/api/json/v1/1/search.php?s=${
+      mealName ? mealName : ''
+    }`
   );
   const getData = await response.json();
 
-  const thambnails = document.getElementById('thambnail');
-  thambnails.innerHTML = '';
-  getData.meals.forEach(meal => {
+  if (getData.meals) {
+    displayAllMeals(getData.meals);
+  } else if (getData.meals) {
+    displayAllMeals('');
+  } else {
+    alert('Not Available');
+  }
+  document.getElementById('spinner').style.display = 'none';
+};
+// display all meals
+const displayAllMeals = meals => {
+  const mealContainer = document.getElementById('meal-container');
+  mealContainer.innerHTML = ``;
+  meals.forEach(meal => {
+    const { strMealThumb: image, strMeal: title, idMeal } = meal;
     const div = document.createElement('div');
     div.innerHTML = `
     <div class="flex gap-4">
-    <img class="w-[250px] h-[250px] rounded-lg object-cover" src="${meal.strMealThumb}"/>
+    <img
+      class="w-[250px] h-[250px] rounded-lg object-cover"
+      src="${image}"
+    />
     <div class="space-y-3">
-      <h2 class="text-lg  font-bold">${meal.strMeal}</h2>
-      <p class="text-sm">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut, fugiat! Commodi exercitationem error  qui  beatae. Nobis nam saepe reprehenderit ratione.</p>
-    <a onclick="modalContent('${meal.strCategory}','${meal.strArea}', '${meal.strInstructions}', '${meal.strYoutube}')" class="underline text-bgButton font-medium" href="#">View Details</a>
-      </div>
+      <h2 class="text-lg  font-bold">${title}</h2>
+      <p class="text-sm">Desserts add a sweet end to meals. Favorites include chocolate cake, pudding, ice cream, and Bengali sweets like rasgulla. They bring joy and a satisfying finish.</p>
+      <a onclick="mealDetails('${idMeal}'); event.preventDefault() "  
+        class="underline text-bgButton font-medium"
+        href="#"
+      >
+        View Details
+      </a>
     </div>
+  </div>
     `;
-    thambnails.appendChild(div);
+    mealContainer.appendChild(div);
   });
 };
 
-const modalContent = (category, area, process, channel) => {
-  const modal = document.getElementById('my_modal_1');
-  modal.querySelector('.modal-box').innerHTML = `
+const handleSearch = () => {
+  document.getElementById('spinner').style.display = 'block';
+  const searchText = document.getElementById('search-box').value;
+
+  setTimeout(() => {
+    loadAllMeals(searchText);
+  }, 3000);
+};
+
+// Meals Details
+const mealDetails = async id => {
+  const response = await fetch(
+    `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
+  );
+  const getData = await response.json();
+  const {
+    strMealThumb: image,
+    strCategory: category,
+    strArea: area,
+    strYoutube: channel,
+  } = getData.meals[0];
+
+  const modalContainer = document.getElementById('modal-container');
+  modalContainer.innerHTML = `
+  <dialog id="my_modal_1" class="modal">
+          <div class="modal-box">
+            <img
+              class="w-full h-[250px] rounded-lg object-cover"
+              src="${image}"
+              />
             <h3 class="text-lg font-bold">Category: ${category}</h3>
-              <p class="py-4">Area: ${area}</p>
-                <p class="py-4">Instructions: ${process}</p>
-                <p class="py-4 text-sm">
-              <a href="${channel}" target="_blank">Youtube: ${channel}</a>
-                </p>
+            <p class="py-4">Area: ${area}</p>
+            <p class="text-sm">
+            <a href="${channel}" target="_blank">Youtube: ${channel}</a>
+            </p>
             <div class="modal-action">
               <form method="dialog">
-                <button class="px-8 py-4 text-white bg-red-500">Close</button>
+                <button class="btn">Close</button>
               </form>
             </div>
+          </div>
+        </dialog>
   `;
-  modal.showModal();
+
+  my_modal_1.showModal();
 };
-searchMealByName();
+
+loadAllMeals('');
